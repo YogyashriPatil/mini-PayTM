797fb563bf447384d8699b7df4896c13900af5a9
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-28 08:39:38 +0530

    Schema completed

--- backend/db.js
+++ backend/db.js
@@ -1,30 +1,42 @@
 import mongoose, { model } from "mongoose";
-import z from "zod"
+import z, { float32 } from "zod"
 mongoose.connect(process.env.MONGODB_URL)
 const userSchema = mongoose.Schema({
     userName: {
         type : z.string(),
-        require: true,
+        required: true,
         unique: true,
     },
     firstName: {
         type : z.string(),
-        require: true,
+        required: true,
         unique: true,
     },
     lastName: {
         type : z.string(),
-        require: true,
+        required: true,
         unique: true,
     },
     password:{
         type:z.string(),
-        require: true
+        required: true
     },
     
 })
-
-const User = mongoose.model("user", userSchema)
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true
+    }
+})
+const Account = mongoose.model("Account", bankSchema)
+const User = mongoose.model("User", userSchema)
 module.exports = {
-    User
+    User,
+    Account
 }
